b380f287def31637b9675aaf2ecbc89a03dba126
ramosramosramos <ramosramosramosramos12@gmail.com>
2024-09-28 19:24:17 +0800

    finishing component field

---
+++ resources/js/Components/TextField.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="mb-3">
+        <label>{{ label }}</label>
+        <input v-model="model" :type="type" :placeholder="placeholder">
+        <p class="text-red-500">{{ error }}</p>
+    </div>
+</template>
+<script setup>
+
+defineProps({
+    type: {
+        type: String,
+        default: "text",
+    },
+    placeholder: {
+        type: String,
+        default: "placeholder",
+    },
+
+    error: {
+        type: String,
+        default: null,
+    }
+
+
+
+})
+
+
+const model = defineModel();
+
+</script>
